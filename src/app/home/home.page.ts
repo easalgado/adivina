384fd4c1d66546f87e4d42dbf2794dca616a61e3
easalgado <202100446@umh.edu.hn>
2022-03-25 19:38:09 -0600

    recuperacion codigo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
+  num:number;
+  
+  mayorMenor: string = '...';
+  
+  numSecret: number = this.numAleatorio(0,100);
+  
+  
+  constructor(public navCtrl: NavController) {
+  }
+  compruebaNumero(){
+  
+  
+  if(this.num)
+  
+  {
+  
+  if(this.numSecret < this.num)
+  
+  {
+  
+  this.mayorMenor = 'menor';
+  
+  }
+  
+  else if(this.numSecret > this.num)
+  
+  {
+  
+  this.mayorMenor = 'mayor';
+  
+  }
+  
+  else{
+  
+  this.mayorMenor = 'igual';
+  
+  }
+  
+  }
+  
+  }
+  
+  numAleatorio(a,b)
+  
+  {
+  
+  return Math.round(Math.random()*(b-a)+parseInt(a));
+  
+  }
+  
+  reinicia(){
 
-  constructor() {}
-
-}
+    // reiniciamos las variables 
+    this.num = null; this.mayorMenor = '...';
+    this.numSecret = this.numAleatorio(0,100);
+    
+    } 
+  }
